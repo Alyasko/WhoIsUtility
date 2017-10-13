a1def481a796662ea88acefc1991dcc9d9628a6d
Alyasko <yasko.alexander@gmail.com>
2017-10-13 21:28:23 +0300

    Working local test.

--- WhoIsUtility/WhoIsUtility.Core/WhoIs.cs
+++ WhoIsUtility/WhoIsUtility.Core/WhoIs.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
@@ -16,31 +19,78 @@ namespace WhoIsUtility.Core
         // http://www.webservicex.net/whois.asmx?WSDL
         public string GetHostInfo(string hostName)
         {
-            var soapEnv = @"<?xml version=""1.0"" encoding=""utf-8""?><soapenv:Envelopexmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/""xmlns:web=""http://www.webservicex.net""><soapenv:Header/><soapenv:Body><web:GetWhoIS><!--Optional:--><web:HostName>yandex.ru</web:HostName></web:GetWhoIS></soapenv:Body></soapenv:Envelope>";
-            var url = "http://www.webservicex.net/whois.asmx";
+            var url = "http://localhost:8080/axis2/services/HelloWorldService";
 
-            var byteData = Encoding.ASCII.GetBytes(soapEnv);
+            var client = new HttpClient();
 
-            var req = (HttpWebRequest) WebRequest.Create(url);
-            req.Method = "POST";
-            req.ContentLength = byteData.Length;
-            req.Headers.Add("SOAPAction", "http://www.webservicex.net/GetWhoIS");
-            req.Accept = "text/xml";
-            req.ContentType = "text/xml; charset=utf-8";
-
-            var soapEnvelopeXml = new XmlDocument();
-            //soapEnvelopeXml.LoadXml();
-
-            using (var strWriter = req.GetRequestStream())
+            var request = new HttpRequestMessage()
             {
-                strWriter.Write(byteData, 0, byteData.Length);
+                RequestUri = new Uri(url),
+                Method = HttpMethod.Post
+            };
+
+            request.Content = new StringContent(File.ReadAllText(@""), Encoding.UTF8, "text/xml");
+            request.Headers.Clear();
+            // client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("text/xml");
+            request.Headers.Add("SOAPAction", "http://www.webservicex.net/GetWhoIS");
+
+            HttpResponseMessage response = client.SendAsync(request).Result;
+            var result = response.Content.ReadAsStringAsync().Result;
+            Debug.WriteLine(result);
+
+            return result;
+        }
+
+        private void TestSoap()
+        {
+            const string url = "http://www.webservicex.net/whois.asmx";
+            const string action = "http://www.webservicex.net/whois.asmx/GetWhoIS";
+
+            XmlDocument soapEnvelopeXml = CreateSoapEnvelope();
+            HttpWebRequest webRequest = CreateWebRequest(url, action);
+
+            InsertSoapEnvelopeIntoWebRequest(soapEnvelopeXml, webRequest);
+
+            string result;
+            using (WebResponse response = webRequest.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(response.GetResponseStream()))
+                {
+                    result = rd.ReadToEnd();
+                }
             }
+        }
 
-            using (var resp = (HttpWebResponse)req.GetResponse())
-            using (var str = resp.GetResponseStream())
-            using (var strReader = new StreamReader(str))
+        private static HttpWebRequest CreateWebRequest(string url, string action)
+        {
+            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(url);
+            webRequest.Headers.Add("SOAPAction", action);
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+        private static XmlDocument CreateSoapEnvelope()
+        {
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            soapEnvelopeXml.LoadXml(@"<?xml version=""1.0"" encoding=""utf-8""?>
+                    <soap:Envelope xmlns:soap=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+                    <soap:Body>
+                    <GetWhoIS xmlns=""http://www.webservicex.net"">
+                      <HostName>yandex.com</HostName>
+                    </GetWhoIS>
+                    </soap:Body>
+                    </soap:Envelope>");
+            return soapEnvelopeXml;
+        }
+
+        private static void InsertSoapEnvelopeIntoWebRequest(XmlDocument soapEnvelopeXml, HttpWebRequest webRequest)
+        {
+            using (Stream stream = webRequest.GetRequestStream())
             {
-                return strReader.ReadToEnd();
+                soapEnvelopeXml.Save(stream);
             }
         }
     }
